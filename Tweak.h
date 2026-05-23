2371c8c05798aa3cd9787748db9e3411c423f036
qq1643240 <111163830+qq1643240@users.noreply.github.com>
2026-05-24 02:58:26 +0800

    Add files via upload

---
+++ Tweak.h
@@ -0,0 +1,10 @@
+#import "RandomHeaders.h"
+#import "AXNView.h"
+
+@interface SBDashBoardNotificationAdjunctListViewController : UIViewController
+@property (nonatomic, retain) AXNView *axnView;
+@end
+
+@interface CSNotificationAdjunctListViewController : UIViewController
+@property (nonatomic, retain) AXNView *axnView;
+@end
